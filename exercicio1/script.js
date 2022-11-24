92f29e1c4fdc76e7c6b42b8c776f2e76a3cd1629
Michelle Lang Baratto <mi.baratto@gmail.com>
2022-11-24 17:37:56 +0100

    exercicio 1

---
+++ exercicio1/script.js
@@ -0,0 +1,7 @@
+const arrayExemple1 = [20, 10, 55]
+const arrayExemple2 = ["bananinha", "chicletinho", "batatinha"]
+const arrayExemple3 = [20, "sapo", 8 == 20]
+
+console.log(arrayExemple1)
+console.log(arrayExemple2)
+console.log(arrayExemple3)
